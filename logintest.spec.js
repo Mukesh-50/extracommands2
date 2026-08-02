7c1b1b0fc7d1a76e79b86c22a41ee8369a9f0538
Mukesh Otwani <mukeshotwani.50@gmail.com>
2026-08-02 08:36:16 +0530

    added new file and modified last file

--- logintest.spec.js
+++ logintest.spec.js
@@ -2,3 +2,7 @@ console.log("Statement1");
 console.log("Statement2");
 console.log("Statement3");
 console.log("Statement4");
+console.log("Statement5");
+console.log("Statement6");
+console.log("Statement7");
+console.log("Statement8");
---
+++ registration.spec.js
@@ -0,0 +1,4 @@
+console.log("Statement9");
+console.log("Statement10");
+console.log("Statement11");
+console.log("Statement12");
